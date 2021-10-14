245881aad6da2315761bece15c284dae1ab5b9d2
Elton Lac <lacelton@yahoo.com>
2021-10-14 09:23:05 -0700

    Finished #29

---
+++ Answers/29-Remove Duplicates from Sorted Array.js
@@ -0,0 +1,10 @@
+var removeDuplicates = (nums) => {
+  let start = 0;
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] != nums[i + 1]) {
+      nums[start] = nums[i];
+      start++;
+    }
+  }
+  return start;
+};
