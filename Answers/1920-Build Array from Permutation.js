2164b8f3056638c58c3864505189a52f68fbaa0b
Elton Lac <lacelton@yahoo.com>
2021-10-15 12:51:18 -0700

    Added second answer

--- Answers/1920-Build Array from Permutation.js
+++ Answers/1920-Build Array from Permutation.js
@@ -1,3 +1,13 @@
+// Answer 1
 var buildArray = (nums) => {
   return nums.map((arr) => nums[arr]);
+};
+
+// Answer 2
+var buildArray = (nums) => {
+  var arr = [];
+  for (let i = 0; i < nums.length; i++) {
+    arr[i] = nums[nums[i]];
+  }
+  return arr;
 };
