dcb3e47211ff2588160e21534eb96b12ab048feb
Elton Lac <lacelton@yahoo.com>
2021-10-21 22:31:02 -0700

    Finished #1

---
+++ Answers/1-Two Sum.js
@@ -0,0 +1,11 @@
+var twoSum = function (nums, target) {
+  let vals = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in vals) {
+      return [vals[target - nums[i]], i];
+    } else {
+      vals[nums[i]] = i;
+    }
+  }
+  return vals;
+};
