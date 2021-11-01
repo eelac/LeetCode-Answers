7e564dce4e062af5ef929344976cbec167540ef2
Elton Lac <lacelton@yahoo.com>
2021-11-01 11:10:00 -0700

    Added second answer

--- Answers/50-Pow.js
+++ Answers/50-Pow.js
@@ -1,4 +1,22 @@
+// Answer 1
 var myPow = (x, n) => {
   let num = Math.pow(x, n);
   return num;
+};
+
+// Answer 2
+var myPow = (x, n) => {
+  if (n === 0) return 1;
+  if (n > 0) return pow(x, n);
+  if (n < 0) return 1 / pow(x, -n);
+};
+
+var pow = (x, n) => {
+  if (n === 1) return x;
+  var num = pow(x, Math.floor(n / 2));
+  if (n % 2 === 0) {
+    return num * num;
+  } else {
+    return x * num * num;
+  }
 };
