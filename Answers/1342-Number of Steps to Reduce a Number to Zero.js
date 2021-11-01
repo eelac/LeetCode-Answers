7914625ea5f88daf7469681cafa8142bfedd2b77
Elton Lac <lacelton@yahoo.com>
2021-11-01 09:28:13 -0700

    Finished #1342

---
+++ Answers/1342-Number of Steps to Reduce a Number to Zero.js
@@ -0,0 +1,13 @@
+var numberOfSteps = (num) => {
+  let count = 0;
+  while (num !== 0) {
+    if (num % 2 === 0) {
+      num = num / 2;
+      count++;
+    } else {
+      num = num - 1;
+      count++;
+    }
+  }
+  return count;
+};
